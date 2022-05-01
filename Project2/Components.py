dba793c31e2c586ff7c5d8e40e44581939695815
Szymon Pietraszek <szymekadr999@gmail.com>
2022-05-01 20:01:58 +0200

    Components functions - draft

---
+++ Project2/Components.py
@@ -0,0 +1,34 @@
+def get_components(adjList):
+    nr = 0
+    comp = []
+    for node in adjList:
+        comp.append(-1)
+    for node in adjList:
+        if comp[node-1] == -1:
+            nr = nr + 1
+            comp[node-1] = nr
+            components_r(nr, node, adjList, comp)
+    components = { i: [] for i in comp }
+    for i in range(len(comp)):
+        components[comp[i]].append(i+1)
+    return components
+
+def components_r(nr, v, adjList, comp):
+    for node in adjList[v]:
+        if comp[node-1] == -1:
+            comp[node-1] = nr
+            components_r(nr, node, adjList, comp)
+
+def print_components(components):
+    print("Lista spojnych skladowych:")
+    for key, value in components.items():
+        print(f"{key}) " + " ".join(map(str, value)))
+
+def get_greatest_component_index(components):
+    max = 0
+    max_id = 0
+    for key, value in components.items():
+        if len(value) > max:
+            max = len(value)
+            max_id = key
+    return max_id
--- Project2/main.py
+++ Project2/main.py
@@ -1,26 +1,39 @@
-from collections import defaultdict
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../Project1')))
 import fileReader as fr
+import AdjacencyList as AL
 
+from collections import defaultdict
 from Sequence import Sequence
 from Graph import Graph
+from Components import *
 
 
 if __name__ == '__main__':
 
     #zad1------
-    seq = [4, 2, 2, 3, 2, 1, 4, 2, 2, 2, 2]
+    # seq = [4, 2, 2, 3, 2, 1, 4, 2, 2, 2, 2]
 
-    ##seq=[4, 4, 3, 1, 2]
+    # ##seq=[4, 4, 3, 1, 2]
 
-    sequence = Sequence(seq)
-    print("Czy zadany ciag graficzny moze reprezentowac graf?",sequence.ifSequenceIsGraph())  ## True if sequence OK
+    # sequence = Sequence(seq)
+    # print("Czy zadany ciag graficzny moze reprezentowac graf?",sequence.ifSequenceIsGraph())  ## True if sequence OK
 
-    adjacencyList = sequence.sequenceToAdjacencyList()
+    # adjacencyList = sequence.sequenceToAdjacencyList()
 
-    print("Lista sasiedztwa na podstawie ciagu graficznego:/n",adjacencyList.convertToAM())
+    # print("Lista sasiedztwa na podstawie ciagu graficznego:/n",adjacencyList.convertToAM())
 
-    # zad2------
-    print("Randomizacja wykonana ", Graph(0, [], 0, adjacencyList).randomize(adjacencyList, 10), " razy")
+    # # zad2------
+    # print("Randomizacja wykonana ", Graph(0, [], 0, adjacencyList).randomize(adjacencyList, 10), " razy")
 
     # top.mainloop()
+
+    # zad3
+    adjacencyList = AL.AdjacencyList(fr.readMatrix("../Project1/ls.txt"))
+    adjList = {int(i + 1): adjacencyList.adjList[i][:] for i in range(len(adjacencyList.adjList))}
+    print(f"\nLista sasiedztwa: {adjList}\n")
+
+    components = get_components(adjList)
+    print_components(components)
+    print(f"Najwieksza spojna skladowa ma numer: {get_greatest_component_index(components)}\n")
