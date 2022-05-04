4a71b7616bde14cb0d30d4e59eb4b214ba7bf02d
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-05-04 13:34:58 +0200

    Create main.py

---
+++ Project3/main.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+import sys
+import os
+import fileReader as fr
+import AdjacencyList as AL
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../Project1')))
+from Sequence import Sequence
+from Graph import Graph
+
+
+if __name__ == '__main__':
+
+    #zad1------
+
+    #random weighted graph
+    vertices=7
+    probability=0.5
+    graph=Graph(vertices,None,probability,None)
+    graph.generateRandomWeighted()
+
+
+    # zad2------
