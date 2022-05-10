afa76d9e54c550bbf035dfa258f30c7dc43308d4
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-05-10 18:09:48 +0200

    Update Graph.py

--- Project2/Graph.py
+++ Project2/Graph.py
@@ -194,40 +194,4 @@ class Graph:
                 return edge.weight
 
 
-    def dijkstra(self, start, end=-1):
-        current = start
-        visited = set()
-        shortest_paths = {start: (None, 0)}
-        sum_weight = 0
-
-        while current != end:
-            visited.add(current)
-            destinations = self.get_neighbours(current)
-            current_edge_weight = shortest_paths[current][1]
-
-            for dist in destinations:
-                weight = self.get_edge_weight(current, dist) + current_edge_weight
-                if dist not in shortest_paths:
-                    shortest_paths[dist] = (current, weight)
-                else:
-                    current_shortest_weight = shortest_paths[dist][1]
-                    if current_shortest_weight > weight:
-                        shortest_paths[dist] = (current, weight)
-
-            next_destinations = dict()
-            for node in shortest_paths:
-                if node not in visited:
-                    next_destinations[node] = shortest_paths[node]
-
-            current = min(next_destinations, key=lambda key: next_destinations[key][1])
-
-        path = []
-        while current is not None:
-            path.append(current)
-            dist = shortest_paths[current][0]
-            if dist is not None:
-                sum_weight += self.get_edge_weight(current, dist)
-            current = dist
-
-        path = path[::-1]
-        return path, sum_weight
+    
