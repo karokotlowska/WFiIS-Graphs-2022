bee679b01c252fb566647b307ded334a51316f35
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-05-12 15:02:38 +0200

    Update fileReader.py

--- Project1/fileReader.py
+++ Project1/fileReader.py
@@ -7,3 +7,11 @@ def readMatrix(filename):
                matrixRow.append(int(el))
             matrix.append(matrixRow)
     return matrix
+
+def readSequence(filename):
+    matrix = []
+    with open(filename, 'r') as f:
+        for line in f:
+            for el in line.split():
+                matrix.append(int(el))
+    return matrix
