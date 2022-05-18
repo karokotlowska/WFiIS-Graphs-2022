c660da408fbb4cf43b3c3f3b58739eacc934a62e
Volodymyr Tymkiv <volodiatymkiv@gmail.com>
2022-05-18 22:10:00 +0200

    added 2 for project 4

---
+++ Project4/Kosaraju.py
@@ -0,0 +1,61 @@
+from itertools import count
+
+
+def kosaraju(adjMatrix):
+
+    n = len(adjMatrix)
+    stack = []
+    scc = []
+    cn = 0
+    visited = [False for i in range(n)]
+
+    for i in range(n):
+        if not visited[i]:
+            dfsStack(i, visited, stack, adjMatrix)
+
+    tAdjMatrix = transponse(adjMatrix)
+    
+    visited = [False for i in range(n)]
+
+    while len(stack) > 0:
+
+        vertex = stack.pop()
+
+        if visited[vertex]:
+            continue
+        
+        cn += 1
+
+        scc.append([])
+        dfsSprint(vertex, visited, tAdjMatrix, scc[cn - 1])
+
+    print(scc)
+
+        
+
+def dfsSprint(vertex, visited, adjMatrix, scc):
+    visited[vertex] = True
+    scc.append(vertex + 1)
+
+    for i in range(len(adjMatrix[vertex])):
+        if adjMatrix[vertex][i] == 1 and visited[i] == False: 
+            dfsSprint(i, visited, adjMatrix, scc)
+
+def dfsStack(vertex, visited, stack, adjMatrix):
+    visited[vertex] = True
+    for i in range(len(adjMatrix[vertex])):
+        if adjMatrix[vertex][i] == 1 and visited[i] == False: 
+            dfsStack(i, visited, stack , adjMatrix)
+
+    stack.append(vertex)
+
+def transponse(adjMatrix):
+    matrix = []
+
+    for i in range(len(adjMatrix)):
+        matrix.append([])
+        for j in range(len(adjMatrix[i])):
+            matrix[i].append(adjMatrix[j][i])
+
+    return matrix
+
--- Project4/main.py
+++ Project4/main.py
@@ -5,13 +5,17 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../Proje
 from Graph import *
 from RandomDirecredGraph import *
 import fileReader as fr
+from Kosaraju import kosaraju
 
 '''Zad1 - Direcred Graph and Random Directed Graph'''
 matrix1=randomDirectedGraph(7,0.3)
 # matrix2=fr.readMatrix('directedGraphExample1.txt')
-
+print('-----zad1-----losowy graf skierowany')
 for row in matrix1:
     print(row)
 
+# ------------ zad 2
+print('\n\n-----zad1-----szukanie silnie spojnych skladowych dla losowego grafu')
+kosaraju(matrix1)
 
 
