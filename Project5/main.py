ce45be55815a390ef497f89a1ba8228ea72872fa
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-05-18 14:42:35 +0200

    Update main.py

--- Project5/main.py
+++ Project5/main.py
@@ -1,1 +1,12 @@
+from FlowNetwork import *
 
+def task1():
+    print('\n\n----------zad1-----------')
+    layers_number = 2
+    network = FlowNetwork(layers_number)
+    matrix = network.flow_network_generator(2 * layers_number)
+    max_flow = ford_fulkerson(matrix, 6)
+    print(max_flow)
+
+if __name__ == '__main__':
+    task1()
