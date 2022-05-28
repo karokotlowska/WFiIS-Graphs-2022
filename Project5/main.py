1aec8fb0047a9ac532ba5171b7e25f4a7a1e0fd2
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-05-28 22:41:21 +0200

    Update main.py

--- Project5/main.py
+++ Project5/main.py
@@ -4,9 +4,11 @@ def task1():
     print('\n\n----------zad1-----------')
     layers_number = 3
     network = FlowNetwork(layers_number)
-    matrix, vertices_count = network.flow_network_generator(2 * layers_number)
+    matrix, vertices_count, edges = network.flow_network_generator(2 * layers_number)
     max_flow = ford_fulkerson(matrix, vertices_count)
     print(max_flow)
+    
+    network.draw_network(edges, max_flow)
 
 if __name__ == '__main__':
     task1()
